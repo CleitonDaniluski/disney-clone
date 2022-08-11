99d29d1c01500eb3319247e6e85108aa8ddb4458
Cleiton Daniluski Legnari <cleiton.daniluski@hotmail.com>
2022-08-11 13:54:56 -0300

    Projeto Disney

---
+++ Pagina Filmes Disney/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Falcão e o Soldado Invernal são obrigados a formar uma dupla incompatível e embarcarem em uma aventura global que deve testar tanto suas habilidades de sobrevivência quanto sua paciência.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Loki, Deus da Trapaça, sai da sombra de seu irmão para embarcar em uma aventura que ocorre após os eventos de "Vingadores: Ultimato".',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Vivendo vidas suburbanas perfeitas, Wanda e Visão começam a suspeitar que nem tudo é o que parece.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Há muito tempo, no mundo de fantasia de Kumandra, humanos e dragões viviam juntos em harmonia. Mas quando uma força maligna ameaçou a terra, os dragões se sacrificaram para salvar a humanidade. Agora, 500 anos depois, o mesmo mal voltou e cabe a uma guerreira solitária, Raya, rastrear o lendário último dragão para restaurar a terra despedaçada e seu povo dividido.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Luca vive aventuras com seu novo melhor amigo, mas a diversão é ameaçada por um segredo: seu amigo é um monstro marinho de outro mundo que fica abaixo da superfície da água.',
+        image: 'images/slider 6.PNG'
+    }
+]
